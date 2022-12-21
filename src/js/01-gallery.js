0cee78ec5768bdb41feb4031e6c4a97ed3a146fd
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-21 18:39:32 +0200

    task1
    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const markUp = galleryItems
+.map(({ preview, original, description }) =>
+`
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+ 
+`)
+.join('');
+
+galleryEl.innerHTML = markUp;
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
