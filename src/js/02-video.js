3e0a209d84e92a86c3677cfe3dd9b3de1ed2bff9
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-22 17:28:49 +0200

    task2
    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,18 @@
 import Player from '@vimeo/player';
-import { throttle } from 'throttle-debounce';
-const iframe = document.querySelector('#vimeo-player');
-const player = new Player(iframe);
+import throttle from 'lodash.throttle';
+var throttle = require('lodash.throttle');
 
-const savedTimeLocalStorage = localStorage.getItem('videoplayer-current-time');
-console.log(savedTimeLocalStorage);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENTTIME_LOCAL = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+  localStorage.setItem(CURRENTTIME_LOCAL, JSON.stringify(currentTime));
+}
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENTTIME_LOCAL));
+const { seconds } = currentTime;
+
+player.setCurrentTime(seconds);
